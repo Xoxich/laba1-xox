5b6cef2dbac701b2e0e19e716023d540dcecf16f
Xoxich <113353408+Xoxich@users.noreply.github.com>
2022-10-03 14:04:06 +0300

    Add files via upload

---
+++ 2new.c
@@ -0,0 +1,15 @@
+﻿#include <stdio.h>
+
+int main()
+{
+	int a, b, c;
+	printf("Enter first number = ");
+	scanf_s("%i", &a);
+	printf("Enter first number = ");
+	scanf_s("%i", &b);
+	printf("Enter third number = ");
+	scanf_s("%i", &c);
+	if (a == b || a == c || b == c) printf("triangle is ravnobedrenniy\n");
+	else printf("triangle is not ravnobedrenniy\n");
+	return 0;
+}
